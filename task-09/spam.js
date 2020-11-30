e049c998597cfff0728235b43f39a4747ddfa849
madhankumar0505 <75214585+madhankumar0505@users.noreply.github.com>
2020-11-30 14:20:54 +0530

    Create spam.js

---
+++ task-09/spam.js
@@ -0,0 +1,17 @@
+//telegram web spamming :
+
+
+var message = ""; 
+var interval = 1  ; 
+var count = 10 ; 
+var notifyInterval = 5 ; 
+var i = 0 ;
+var timer = setInterval(function(){
+	document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = message;
+	$('.im_submit').trigger('mousedown');	
+	i++;
+	if( i  == count )
+	clearInterval(timer);
+	if( i % notifyInterval == 0)
+	console.log(i + ' MESSAGES SENT');
+} , interval * 1000 ) ;
